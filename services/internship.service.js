4c5bd5bb52475fe12d01e002c9e3f80851cbdeca
najlase <44806328+najlase@users.noreply.github.com>
2020-11-13 11:21:13 +0100

    Create internship.service.js

---
+++ services/internship.service.js
@@ -0,0 +1,11 @@
+const Internship = require("../models/Internships");
+
+function InternshipService() {
+    async function getInternships() {
+      return Internship.find({});
+    }
+
+    return {getInternships};
+}
+
+module.exports = userService;
