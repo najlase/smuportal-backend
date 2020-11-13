0ebf6c57b37d0f28c40d77ae67fe4a559d418f79
nadiabenslima <nadia.benslima@medtech.tn>
2020-11-13 12:44:54 +0100

    internship api

--- routes/api/internship.js
+++ routes/api/internship.js
@@ -14,26 +14,5 @@ const router = Router({
     }
   });
 
-  //Route to create an internship
-router.post("/addInternship", async(req, res, next) => {
-  try {
-      const {title, author, isbn} = req.body;
-      await bookService.addBook(title, author, isbn);
-      res.send({ success: true, msg: "Book Added"});
-  } catch (err) {
-      res.send({ success: false, msg: "Book not Added!", err})
-  }
-});
-
-// Route to delete an internship
-router.delete("/deleteInternship/:isbn", async(req, res) => {
-  try {
-    const isbn = req.params.isbn;
-    internshipService.deleteInternship(isbn);
-    res.send({ success: true, msg: "Internship deleted"})
-  } catch (error) {
-    res.send({ success: false, msg: "Internship not added!"})
-  }
-});
 
 module.exports = router;
