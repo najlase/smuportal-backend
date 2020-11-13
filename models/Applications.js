51741362b8e4f6e0957e2fb0e9363f6f3fd47b94
Jihene-ch <jihene.chrf@gmail.com>
2020-11-13 05:01:10 +0100

    First Commit

---
+++ models/Applications.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const ApplicationsSchema = new mongoose.Schema({
+Files: {
+    type: file,
+    required: true,
+    lowercase: true,
+    min: 2,
+    max: 255
+  },
+AppliedOn: {
+    type: Date,
+    required: true,
+    lowercase: true,
+    min: 2,
+    max: 255
+  },
+StdID: {
+    type: Number,
+    unique: true,
+    required: true,
+    min: 1000000000,
+    max: 9999999999999,
+    validate: {
+      validator: Number.isInteger,
+      message: "{VALUE} is not an integer value"
+    }
+  },
+InternshipID: {
+    type: Number,
+    unique: true,
+    required: true,
+    min: 1000000000,
+    max: 9999999999999,
+    validate: {
+      validator: Number.isInteger,
+      message: "{VALUE} is not an integer value"
+    }
+  },
+
+  Status: {
+    type: String,
+    required: true,
+    wercase: true,
+    min: 2,
+    max: 255
+  },
+});
+
+module.exports = mongoose.model("Book", bookSchema);
