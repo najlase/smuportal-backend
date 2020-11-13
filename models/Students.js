01bb76dd7ce6ab444713c111e4f808509de1c035
Jihene-ch <jihene.chrf@gmail.com>
2020-11-13 05:10:52 +0100

    First Commit

---
+++ models/Students.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const studentsSchema = new mongoose.Schema({
+  Name: {
+    type: String,
+    required: true,
+    lowercase: true,
+    min: 2,
+    max: 255
+  },
+  InternshipID: {
+    type: Number,
+    unique: true,
+    required: true,
+    min: 1000000000,
+    max: 9999999999999,
+    validate: {
+      validator: Number.isInteger,
+      message: "{VALUE} is not an integer value"
+    }
+  },
+  StdID: {
+    type: Number,
+    unique: true,
+    required: true,
+    min: 1000000000,
+    max: 9999999999999,
+    validate: {
+      validator: Number.isInteger,
+      message: "{VALUE} is not an integer value"
+    }
+  },
+
+});
+
+module.exports = mongoose.model("Students", StudentsSchema);
