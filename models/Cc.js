d4585143ce8441c22192b83b6f47b9471baef1b2
Jihene-ch <jihene.chrf@gmail.com>
2020-11-13 05:11:23 +0100

    First Commit

---
+++ models/Cc.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const ApplicationsSchema = new mongoose.Schema({
+Status: {
+    type: String,
+    required: true,
+    lowercase: true,
+    min: 2,
+    max: 255
+  },
+CcID: {
+    type: Number,
+    unique: true,
+    required: true,
+    min: 1000000000,
+    max: 9999999999999,
+    validate: {
+      validator: Number.isInteger,
+      message: "{VALUE} is not an integer value"
+    }
+  },
+
+  Validation: {
+    type: String,
+    required: true,
+    wercase: true,
+    min: 2,
+    max: 255
+  },
+});
+
+module.exports = mongoose.model("Cc", CcSchema);
