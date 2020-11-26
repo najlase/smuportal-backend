0eabba7eed696c456deb5761b6518283898e00b0
najlase <44806328+najlase@users.noreply.github.com>
2020-11-26 17:52:32 +0100

    delete internship service

--- services/internship.service.js
+++ services/internship.service.js
@@ -2,24 +2,28 @@ const Internship = require("../models/Internship");
 const mongoose = require("mongoose");
 
 class InternshipService {
-    async getInternships() {
-      return Internship.find({});
-    }
+  async getInternships() {
+    return Internship.find({});
+  }
 
-    async createInternship(internshipData) {
-     internshipData.Company = mongoose.Schema.Types.ObjectId(internshipData.Company);
-      const internship = new Internship(internshipData);
-      await internship.save();
-      return internship;
-    }
+  async createInternship(internshipData) {
+    internshipData.Company = mongoose.Schema.Types.ObjectId(internshipData.Company);
+    const internship = new Internship(internshipData);
+    await internship.save();
+    return internship;
+  }
 
-    async updateInternship(id, internshipData) {
-      if (internshipData.Company)
-       internshipData.Company = mongoose.Schema.Types.ObjectId(internshipData.Company);  
+  async updateInternship(id, internshipData) {
+    if (internshipData.Company)
+      internshipData.Company = mongoose.Schema.Types.ObjectId(internshipData.Company);
 
-     let internship = await Internship.update({_id: id}, internshipData, { multi: true });
-       return internship;
-     }
+    let internship = await Internship.update({ _id: id }, internshipData, { multi: true });
+    return internship;
+  }
+
+  async deleteInternship(id) {
+    await Internship.deleteOne({ _id: id });
+  }
 }
 
 module.exports = new InternshipService();
