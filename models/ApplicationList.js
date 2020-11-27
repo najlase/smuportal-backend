d45a7fec2490dd2d9271a14cc4d9e03229b6752a
Jihene-ch <jihene.chrf@gmail.com>
2020-11-27 09:47:51 +0100

    Defined the schema for the list of application that cc gets

---
+++ models/ApplicationList.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const ApplicationListSchema = new mongoose.Schema({
+    _ApllicationId: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "MyApplication"
+        }
+    ],
+    //Files
+    Files:{
+        type:String,
+        data:Buffer
+    },
+    //Validation
+    Valid:{
+        type: String,
+        lowercase: true,
+        enum: ["valid", "invalid","none"],
+        default: "none"
+    },
+    //Status
+    Status:{
+        type: String,
+        lowercase: true,
+        enum: ["pending", "submitted to company", "received by company"],
+        default: "pending"
+    }
+});
+module.exports = mongoose.model("ApplicationList", ApplicationListSchema);
